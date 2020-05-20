b48292fcf4b3593b2fe11addefc6073df524d623
Guillaume Seznec <guillaume@seznec.fr>
2020-05-20 10:15:17 +0200

    doc

--- mkloop.sh
+++ mkloop.sh
@@ -1,3 +1,8 @@
+##
+# Génère une version bouclable de la vidéo
+# en produisant une version reverse et en la concaténant à la vidéo d'origine
+##
+
 ffmpeg -i dynamic-background.mkv -vf reverse -crf 18 dynamic-background-reverse.mkv
 for f in dynamic-background.mkv dynamic-background-reverse.mkv; do echo "file '$f'" >> concat.txt; done
 ffmpeg -f concat -safe 0 -i concat.txt -c copy dynamic-background-loop.mkv
--- rec.sh
+++ rec.sh
@@ -1,1 +1,5 @@
-ffmpeg -video_size 1920x1080 -framerate 25 -f avfoundation -pix_fmt uyvy422 -i "0:" -pix_fmt yuv420p -r 25 -s 1280x720 -c:v libx264 -crf 18 dynamic-background.mkv
+##
+# Enregistre l'écran pendant 5 minutes
+##
+
+ffmpeg -t 00:05:00 video_size 1920x1080 -framerate 25 -f avfoundation -pix_fmt uyvy422 -i "0:" -pix_fmt yuv420p -r 25 -s 1280x720 -c:v libx264 -crf 18 dynamic-background.mkv
